be36d36ef0f97eaf3b7d4e974a391416387adda2
yoozze <ezzooy@gmail.com>
2016-12-24 00:37:03 +0100

    Fix install script

--- scripts/install.js
+++ scripts/install.js
@@ -1,16 +1,40 @@
 var exec = require('child_process').exec;
 var colors = require('colors');
 
-// Download Composer
-console.log('Downloading local Composer...'.yellow);
-exec('php -r "readfile(\'https://getcomposer.org/installer\');" | php', function (error, stdout) {
-  console.log('Finished downloading local Composer!'.green);
-  console.log('Installing Composer dependencies... (this may take a while)'.yellow);
-  exec('php ./composer.phar install --no-dev', function (error, stdout) {
-    console.log('Finished installing Composer dependencies!'.green);
-    console.log('Deleting local Composer...'.yellow);
-    exec('rm -f ./composer.phar', function () {
-      console.log('Finished deleting local Composer!'.green);
+// Download and instal Composer and dependencies
+console.log('Downloading Composer...'.yellow);
+exec('php -r "copy(\'https://getcomposer.org/installer\', \'composer-setup.php\');"', function (error, stdout) {
+  if (error) {
+    console.log(error);
+    return;
+  }
+
+  console.log('Finished downloading Composer!'.green);
+  console.log('Installing Composer...'.yellow);
+  exec('php composer-setup.php', function (error, stdout) {
+    if (error) {
+      console.log(error);
+      return;
+    }
+
+    console.log('Finished installing Composer!'.green);
+    console.log('Installing Composer dependencies...'.yellow);
+    exec('php ./composer.phar install --no-dev', function (error, stdout) {
+      if (error) {
+        console.log(error);
+        return;
+      }
+
+      console.log('Finished installing Composer dependencies!'.green);
+      console.log('Deleting Composer...'.yellow);
+      exec('php -r "unlink(\'composer-setup.php\'); unlink(\'composer.phar\');"', function (error, stdout) {
+        if (error) {
+          console.log(error);
+          return;
+        }
+
+        console.log('Finished deleting Composer!'.green);
+      });
     });
   });
 });
